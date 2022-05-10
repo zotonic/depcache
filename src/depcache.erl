c25fb1ddc3ba1f772ff651e7625c16b12c5f6b8d
Anatolii Kosorukov <java1cprog@yandex.ru>
2022-05-10 18:24:41 +0300

    Mix style documentation
    Add an option.
    Documentation generation based on [hex.pm](https://hex.pm/) publishing documentation recomendations using [rebar3_ex_doc](https://hexdocs.pm/rebar3_ex_doc/readme.html) functionality.

--- src/depcache.erl
+++ src/depcache.erl
@@ -1,6 +1,6 @@
 %% @author Arjan Scherpenisse
 %% @copyright 2009-2020 Marc Worrell, Arjan Scherpenisse
-%% @doc 
+%% @doc Depcache API
 %%
 %% == depcache API ==
 %% {@link flush/1}, {@link flush/2}, {@link get/2},	{@link get/3}, {@link get_subkey/3}, 
@@ -821,7 +821,7 @@ handle_cast(_Msg, State) ->
     
 %% @doc This function is called by a `gen_server' process when when it receives `tick' or
 %% any other message than a synchronous or asynchronous request (or a system message).
-%% @see gen_server:handle_info/2
+%% @private
 
 -spec handle_info(Info, State) -> Result when 
     Info :: tick | any(),
@@ -838,7 +838,7 @@ handle_info(_Msg, State) ->
 
 
 %% @doc This function is called by a `gen_server' process when it is about to terminate.
-%% @see gen_server:terminate/2
+%% @private
 
 -spec terminate(Reason, State) -> Result when
     Reason :: normal | shutdown | {shutdown, term()} | term(),
@@ -850,7 +850,7 @@ terminate(_Reason, _State) -> ok.
 
 %% @doc This function is called by a gen_server process when it is to update 
 %% its internal state during a release upgrade/downgrade.
-%% @see gen_server:code_change/3
+%% @private
 
 -spec code_change(OldVersion, State, Extra) -> Result when
 	OldVersion :: (term() | {down, term()}),
